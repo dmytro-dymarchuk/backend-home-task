7b4b1b67825d222de56d95b6ff4ade7ab61d7a3e
Dmytro Dymarchuk <mr.dymarchuk@gmail.com>
2022-07-12 18:56:19 +0300

    fix setting up

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,6 +5,8 @@ set -e
 echo "xdebug.mode=debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 echo "xdebug.client_host=host.docker.internal" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
 
+composer install
+
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
-ip -4 route list match 0/0 | awk '{print $$3" host.docker.internal"}' >> /etc/hosts && php-fpm --allow-to-run-as-root
+exec "$@"
